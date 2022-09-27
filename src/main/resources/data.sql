d1bdc44632dd1fc68ad907f00e37a628e29060ba
dev <resulozel@gmail.com>
2022-09-27 21:19:21 +0300

    data.sql file modified

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
-INSERT INTO book (id, title , author) VALUES (1, 'USA' , 'USA');
-INSERT INTO book (id, title , author) VALUES (2, 'France','France');
-INSERT INTO book (id, title , author) VALUES (3, 'Brazil', 'Brazil');
-INSERT INTO book (id, title , author) VALUES (4, 'Italy' , 'Brazil');
-INSERT INTO book (id, title , author) VALUES (5, 'Canada' , 'Brazil');
+INSERT INTO book (id, title , author) VALUES (1, 'Silahlara Veda' , 'Ernest Hemingway');
+INSERT INTO book (id, title , author) VALUES (2, 'Savaş ve Barış','Tolstoy');
+INSERT INTO book (id, title , author) VALUES (3, 'Yaşlı Adam ve Deniz', 'Ernest Hemingway');
+INSERT INTO book (id, title , author) VALUES (4, 'Savaş ve Barış' , 'Tolstoy');
+INSERT INTO book (id, title , author) VALUES (6, 'Kadınlar' , 'Ernest Hemingway');
+INSERT INTO book (id, title , author) VALUES (7, 'Savaş ve Barış' , 'Tolstoy');
